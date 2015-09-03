206facedc1b6442a8cca78586c99ed40122d0088
lea3182 <lkim3182@gmail.com>
2015-09-02 19:38:16 -0700

    Create user index and show action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_member!
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
